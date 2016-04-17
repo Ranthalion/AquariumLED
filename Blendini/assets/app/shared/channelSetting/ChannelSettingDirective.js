fcb33a53e9ccda6722bc2d8639689c7806f248c7
Ranthalion <Ranthalion@hotmail.com>
2016-04-16 22:42:03 -0400

    adding directive

---
+++ Blendini/assets/app/shared/channelSetting/ChannelSettingDirective.js
@@ -0,0 +1,38 @@
+(function(){
+	angular.module('channelSetting', []).directive('channelSetting', function(){
+		
+		return {
+			restrict: 'EA',
+			scope: {
+				setting: '='
+			},
+			controller:  function($scope, $timeout, Channel){
+				
+				var timeout = null;
+				var saveUpdates = function() {
+					if ($scope['form_' + $scope.$id].$valid) {
+						Channel.update($scope.setting);
+					} else {
+						toastr.error('Unable to save settings for Port ' + $scope.setting.port + ' because it is invalid.');
+					}
+				};
+				var debounceUpdate = function(newVal, oldVal) {
+					if (newVal != oldVal) {
+						if (timeout) {
+							$timeout.cancel(timeout);
+						}
+						timeout = $timeout(saveUpdates, 2000);
+					}
+				};
+				$scope.$watch('setting.color', debounceUpdate);
+				$scope.$watch('setting.red', debounceUpdate);
+				$scope.$watch('setting.green', debounceUpdate);
+				$scope.$watch('setting.blue', debounceUpdate);
+			},
+			link: function(scope, element, attrs, controllers){
+
+			},
+			templateUrl: 'app/shared/channelSetting/ChannelSettingView.html'
+		};
+	});
+}());
