0112be29debc1e1cdaf630d8ca9cb784f193a9d3
Ranthalion <Ranthalion@hotmail.com>
2016-10-05 20:38:02 -0400

    Disable PWM when channel set to 0

--- PWMDriver/PWMDriver/pwm.c
+++ PWMDriver/PWMDriver/pwm.c
@@ -1,51 +1,102 @@
 #include <avr/io.h>
 #include "pwm.h"
 #include "serial.h"
+#include "leds.h"
 
 void pwm_init()
 {
 	//Time 0 PWM set up
-	TCCR0A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20); //Enable non inverting 8-Bit Fast PWM on A and B
+	TCCR0A = _BV(WGM21) | _BV(WGM20); //Enable non inverting 8-Bit Fast PWM on A and B
 	TCCR0B = _BV(CS20); //clock without any prescaler
 	
 	//Timer 1 PWM set up
-	TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM10);
+	TCCR1A = _BV(WGM10);
 	TCCR1B = _BV(WGM12) | _BV(CS10);
 	
 	//Timer 2 PWM set up
-	TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20); //Enable non inverting 8-Bit Fast PWM on A and B
+	TCCR2A = _BV(WGM21) | _BV(WGM20); //Enable non inverting 8-Bit Fast PWM on A and B
 	TCCR2B = _BV(CS20); //clock without any prescaler
-		
+
 	DDRB |= _BV(PORTB1) | _BV(PORTB2) | _BV(PORTB3);  //Enable output for OC1 and OC2A
 	DDRD |= _BV(PORTD3) | _BV(PORTD5) | _BV(PORTD6);  //Enable output for OC0 and OC2B
 
-	//TODO: [ML] Add initialization code to keep them all off;
 }
 
 void set_channel(uint8_t channel, uint8_t val)
 {	
 	if(channel == 0)
 	{
-		OCR0A = val;
+		if (val == 0){
+			TCCR0A &= ~_BV(COM0A1);
+		}
+		else
+		{
+			TCCR0A |= _BV(COM0A1);
+		}
+		OCR0A = val;	
 	}
 	else if (channel == 1)
 	{
+		if (val == 0)
+		{
+			TCCR0A &= ~_BV(COM0B1);
+		}
+		else
+		{
+			TCCR0A |= _BV(COM0B1);
+		}
 		OCR0B = val;
+		
 	}
 	else if (channel == 2)
 	{
-		OCR1A = val;
+		if (val == 0)
+		{
+			TCCR1A &= ~_BV(COM1A1);			
+		}
+		else
+		{
+			TCCR1A |= _BV(COM1A1);
+		}
+		OCR1A = val;	
+		
 	}
 	else if (channel == 3)
 	{
+		if (val == 0)
+		{
+			TCCR1A &= ~_BV(COM1B1);
+		}
+		else
+		{
+			TCCR1A |= _BV(COM1B1);			
+		}
 		OCR1B = val;
+		
 	}
 	else if (channel == 4)
 	{
+		if (val == 0)
+		{
+			TCCR2A &= ~_BV(COM2A1);
+		}
+		else
+		{
+			TCCR2A |= _BV(COM2A1);
+		}
 		OCR2A = val;
+		
 	}
 	else if (channel == 5)
 	{
+		if (val == 0)
+		{
+			TCCR2A &= ~_BV(COM2B1);
+		}
+		else
+		{
+			TCCR2A |= _BV(COM2B1);
+		}
 		OCR2B = val;
 	}
 	
@@ -53,12 +104,25 @@ void set_channel(uint8_t channel, uint8_t val)
 
 void set_all_channels(uint8_t val)
 {
-	OCR0A = val;
-	OCR0B = val;
-	OCR1A = val;
-	OCR1B = val;
-	OCR2A = val;
-	OCR2B = val;	
+	if (val == 0)
+	{
+		TCCR0A &= ~(_BV(COM0A1) | _BV(COM0B1));
+		TCCR1A &= ~(_BV(COM1A1) | _BV(COM1B1));
+		TCCR2A &= ~(_BV(COM2A1) | _BV(COM2B1));
+	}
+	else
+	{
+		TCCR0A |= _BV(COM0A1) | _BV(COM0B1);
+		TCCR1A |= _BV(COM1A1) | _BV(COM1B1);
+		TCCR2A |= _BV(COM2A1) | _BV(COM2B1);	
+	
+		OCR0A = val;
+		OCR0B = val;
+		OCR1A = val;
+		OCR1B = val;
+		OCR2A = val;
+		OCR2B = val;	
+	}
 }
 
 uint8_t read_channel(uint8_t channel)
